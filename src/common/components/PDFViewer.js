cd27693213e606b53d3af6e60317bfbb0dd56882
soamariaka <hei.soa.2@gmail.com>
2023-08-03 10:20:39 +0300

    feat: pdf viewer

---
+++ src/common/components/PDFViewer.js
@@ -0,0 +1,16 @@
+import { Document, Page } from 'react-pdf';
+import useWindowSize from '../hooks'
+
+export const PDFViewer = ({ pdfByteArray }) => {
+  const windowSize = useWindowSize();
+  const pdfWidth = Math.floor(windowSize.width * (2 / 3)); 
+  const pdfHeight = Math.floor(windowSize.height * (2 / 3));
+
+  return (
+    <div>
+      <Document file={pdfByteArray}>
+        <Page width={pdfWidth} height={pdfHeight} pageNumber={1} />
+      </Document>
+    </div>
+  );
+};
