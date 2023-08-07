9ed6073b7d287e13f4a4bc7e251fe4e41b41b517
Zoherifitia <hei.nitsiaro@gmail.com>
2023-08-07 14:52:34 +0300

    feat: late fees icone

--- src/common/utils/RenderIsPaid.js
+++ src/common/utils/RenderIsPaid.js
@@ -4,29 +4,18 @@ import ErrorIcon from '@mui/icons-material/Error';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 function getIconAndColor(record) {
-  let icon;
-  let color;
 
   if (record.status === 'LATE') {
-    icon = <ErrorIcon style={{ color: mainTheme.palette.error.light }} />;
-    color = 'red';
-  } else if (record.status === 'PAID') {
-    icon = <CheckCircleIcon style={{ color: 'green' }} />;
-    color = 'yellow';
-  } else {
-    icon = null;
-    color = 'inherit';
-  }
-
-  return { icon, color };
+    return <ErrorIcon style={{ color: mainTheme.palette.error.light }} />;
+  } 
 }
 
 
-export const RenderIsPaid = ({ record }) => {
-    const { icon, color } = getIconAndColor(record);
+export const RenderIsPaid = (record) => {
+    const icon= getIconAndColor(record);
   
     return (
-      <div style={{ color, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         {icon}
       </div>
     );
